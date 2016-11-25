64c1767cc5703575fad6729c7c62d2c71171e83f
Alex <o0o.moro.o0o@gmail.com>
2016-11-25 16:53:56 -0500

    initial implmentation of action choice from network

--- policygradient.py
+++ policygradient.py
@@ -7,23 +7,29 @@ class PolicyGradient:
     def __init__(self, rollout, size):
         self.rollout = rollout
         # create the nn for policy network
-        x = tf.placeholder(tf.float32, [None, size[0]], name="x")
         lastLayer = -1
         x_in = 0
         for layer in size:
-            w = tf.Variable(tf.random.uniform([lastLayer, layer], minval = -1*self.initWeightVal, maxval=self.initWeightVal))
-            b = tf.Variable(tf.random_uniform
-            y = tf.sigmoid(tf.matmul(x_in, w)+b)
+            y=0 # need to define outside of if-block
+            if lastLayer >=0:
+                w = tf.Variable(tf.random.uniform([lastLayer, layer], minval = -1*self.initWeightVal, maxval=self.initWeightVal))
+                b = tf.Variable(tf.random_uniform
+                y = tf.sigmoid(tf.matmul(x_in, w)+b)
+            else:
+                y = tf.placeholder(tf.float32, [None, layer], name="x")
             lastLayer = layer
             x_in = y
         self.output = x_in
-        self.probability = output/tf.reduce_sum(self.output)
+        self.probability = output/tf.reduce_sum(self.output) # may need to change this.
 
         learningRate = 0.1
         self.optimizer = tf.train.GradientDescentOptimizer(learningRate)
 
     def doAction(observation):
         #roll dice, then select based on our probability distribution
+        probs = self.probability.eval({x: observation})
+        roll = np.random.choice( len(probs), dimensionality, p=probs)
+        return roll
         
 
     def train(self, size, stepsize):
